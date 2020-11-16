32d78773e9cb061c526dcfc7847bd4bf4c9e9401
shakirov-vm <70662311+shakirov-vm@users.noreply.github.com>
2020-11-16 23:38:57 +0300

    Update RevSort.h

--- RevSort.h
+++ RevSort.h
@@ -3,23 +3,81 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <ctype.h>
+#include <assert.h>
 
-
-int RevCompare (char* line_1, char* line_2, int len_1, int len_2)
+void PrintSorted(struct LineInfo *Line, int SumString)
 {
-    int k = 0;
+    int j = 0;
+    for(j; j <= SumString; j++)
+    {
+        if (*Line[j].index != '\n')
+        {
+            printf("%s\n", Line[j].index);
+        }
+    }
+}
+int Compare (struct LineInfo line_1, struct LineInfo line_2)
+{
+    //printf("Line_1 - <%s>, len_1 - <%d>\n", line_1, len_1);
+    //printf("Line_2 - <%s>, len_2 - <%d>\n", line_2, len_2);
 
-    int i = len_1;
-    int j = len_2;
+    if (line_1.len == -1)    return  1;
+    if (line_2.len == -1)    return -1;
 
     int differ = 0;
 
-    for (k = 0; (i != 0) && (len_2 != 0); k++)
-    {
-        while (isalpha(line_1[i]) == 0) i--;
-        while (isalpha(line_2[j]) == 0) j--;
+    int k = 0;
 
-        differ = line_1[i] - line_2[j];
+    int i = 0;
+    int j = 0;
+
+    ///while (isalpha(line_1[i]) == 0) i++;
+    ///while (isalpha(line_2[j]) == 0) j++;
+///                        +1?
+    for (k = 0; (i != line_1.len) && (j != line_2.len); k++)
+    {
+        while ((isalpha(*(line_1.index + i)) == 0)) i++;
+        while ((isalpha(*(line_2.index + j)) == 0)) j++;
+
+        if(i > line_1.len) break;
+        if(j > line_2.len) break;
+
+        differ = *(line_1.index + i) - *(line_2.index + j);
+
+        i++;
+        j++;
+
+        if (differ != 0) return differ;
+    }
+    return 0;
+}
+int RevCompare (struct LineInfo line_1, struct LineInfo line_2)
+{
+    //printf("Line_1 - <%s>, len_1 - <%d>\n", line_1, len_1);
+    //printf("Line_2 - <%s>, len_2 - <%d>\n", line_2, len_2);
+
+    int differ = 0;
+
+    if (line_1.len == -1)    return  1;
+    if (line_2.len == -1)    return -1;
+
+    int k = 0;
+
+    int i = line_1.len;
+    int j = line_2.len;
+
+    while (isalpha(*(line_1.index + i)) == 0) i--;
+    while (isalpha(*(line_2.index + j)) == 0) j--;
+
+    for (k = 0; (i != -1) && (j != -1); k++)
+    {
+        while (isalpha(*(line_1.index + i)) == 0) i--;
+        while (isalpha(*(line_2.index + j)) == 0) j--;
+
+        if(i < 0) break;
+        if(j < 0) break;
+
+        differ = *(line_1.index + i) - *(line_2.index + j);
 
         i--;
         j--;
@@ -28,50 +86,8 @@ int RevCompare (char* line_1, char* line_2, int len_1, int len_2)
     }
     return 0;
 }
-#if 0
-void RevqsoRT (struct LineInfo * Line, int b, int e)
-{
-    int l = b;
-    int r = e;
-    int change = 0;
-    char* CHANGE = "";
 
-    char* piv_i = Line[(l + r) / 2].index;
-    int piv_l = Line[(l + r) / 2].len;
-
-    while (l <= r)
-    {
-        while (RevCompare(Line[l].index, piv_i, Line[l].len, piv_l) < 0)
-        {
-            l++;
-        }
-        while (RevCompare(piv_i, Line[r].index, piv_l, Line[r].len) < 0)
-        {
-            r--;
-        }
-
-        if (l <= r)
-        {
-            CHANGE = Line[l].index;
-            Line[l].index = Line[r].index;
-            Line[r].index = CHANGE;
-
-            change = Line[l].len;
-            Line[l].len = Line[r].len;
-            Line[r].len = change;
-
-            l = l + 1;
-            r = r - 1;
-        }
-    }
-    if (b < r)
-        RevqsoRT (Line, b, r);
-    if (e > l)
-        RevqsoRT (Line, l, e);
-}
-#endif
-
-void RevqsoRT (struct LineInfo * Line, int Begin, int End/*, int (*Comp) (char*, char*, int, int)*/)
+void qsoRT (struct LineInfo * Line, int Begin, int End, int (*Comp) (struct LineInfo, struct LineInfo))
 {
     int left = Begin;
     int right = End;
@@ -84,12 +100,12 @@ void RevqsoRT (struct LineInfo * Line, int Begin, int End/*, int (*Comp) (char*,
     char* change_index = "";
 
     while (left <= right)
-    { ///      RevCompare заменить на Comp
-        while (RevCompare(Line[left].index, pivor.index, Line[left].len, pivor.len) < 0)
+    {
+        while (Comp(Line[left], pivor) < 0)
         {
             left++;
         }
-        while (RevCompare(pivor.index, Line[right].index, pivor.len, Line[right].len) < 0)
+        while (Comp(pivor, Line[right]) < 0)
         {
             right--;
         }
@@ -109,29 +125,42 @@ void RevqsoRT (struct LineInfo * Line, int Begin, int End/*, int (*Comp) (char*,
         }
     }
     if (Begin < right)
-        RevqsoRT (Line, Begin, right/*, Comp*/);
+        qsoRT (Line, Begin, right, Comp);
+
+
+
     if (End > left)
-        RevqsoRT (Line, left, End/*, Comp*/);
+        qsoRT (Line, left, End, Comp);
 }
 
 
 
-
-
-/*
+#if 0
 int RevCompare (char* line_1, char* line_2, int len_1, int len_2)
 {
+    //printf("Line_1 - <%s>, len_1 - <%d>\n", line_1, len_1);
+    //printf("Line_2 - <%s>, len_2 - <%d>\n", line_2, len_2);
+
+    int differ = 0;
+
+    if (len_1 == -1)    return  1;
+    if (len_2 == -1)    return -1;
+
     int k = 0;
 
     int i = len_1;
     int j = len_2;
 
-    int differ = 0;
+    while (isalpha(line_1[i]) == 0) i--;
+    while (isalpha(line_2[j]) == 0) j--;
 
-    for (k = 0; (i != 0) && (len_2 != 0); k++)
+    for (k = 0; (i != -1) && (j != -1); k++)
     {
         while (isalpha(line_1[i]) == 0) i--;
         while (isalpha(line_2[j]) == 0) j--;
+
+        if(i < 0) break;
+        if(j < 0) break;
 
         differ = line_1[i] - line_2[j];
 
@@ -141,4 +170,86 @@ int RevCompare (char* line_1, char* line_2, int len_1, int len_2)
         if (differ != 0) return differ;
     }
     return 0;
-}*/
+}
+
+void qsoRT (struct LineInfo * Line, int Begin, int End, int (*Comp) (char*, char*, int, int))
+{
+    int left = Begin;
+    int right = End;
+
+    struct LineInfo pivor;
+    pivor.index = Line[(left + right) / 2].index;
+    pivor.len = Line[(left + right) / 2].len;
+
+    int change_len = 0;
+    char* change_index = "";
+
+    while (left <= right)
+    {
+        while (Comp(Line[left].index, pivor.index, Line[left].len, pivor.len) < 0)
+        {
+            left++;
+        }
+        while (Comp(pivor.index, Line[right].index, pivor.len, Line[right].len) < 0)
+        {
+            right--;
+        }
+
+        if (left <= right)
+        {
+            change_index = Line[left].index;
+            Line[left].index = Line[right].index;
+            Line[right].index = change_index;
+
+            change_len = Line[left].len;
+            Line[left].len = Line[ right].len;
+            Line[right].len = change_len;
+
+            left++;
+            right--;
+        }
+    }
+    if (Begin < right)
+        qsoRT (Line, Begin, right, Comp);
+
+
+
+    if (End > left)
+        qsoRT (Line, left, End, Comp);
+}
+int Compare (char* line_1, char* line_2, int len_1, int len_2)
+{
+    //printf("Line_1 - <%s>, len_1 - <%d>\n", line_1, len_1);
+    //printf("Line_2 - <%s>, len_2 - <%d>\n", line_2, len_2);
+
+    if (len_1 == -1)    return  1;
+    if (len_2 == -1)    return -1;
+
+    int differ = 0;
+
+    int k = 0;
+
+    int i = 0;
+    int j = 0;
+
+    ///while (isalpha(line_1[i]) == 0) i++;
+    ///while (isalpha(line_2[j]) == 0) j++;
+///                        +1?
+    for (k = 0; (i != len_1) && (j != len_2); k++)
+    {
+        while ((isalpha(line_1[i]) == 0)) i++;
+        while ((isalpha(line_2[j]) == 0)) j++;
+
+        if(i > len_1) break;
+        if(j > len_2) break;
+
+        differ = line_1[i] - line_2[j];
+
+        i++;
+        j++;
+
+        if (differ != 0) return differ;
+    }
+    return 0;
+}
+#endif
